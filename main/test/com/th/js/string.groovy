f32d5888d1ec485877521a3022a1c58964de21f2
lyrieek <tianhao7@foxmail.com>
2018-03-29 10:53:07 +0800

    mark util: Property

--- main/test/com/th/js/string.groovy
+++ main/test/com/th/js/string.groovy
@@ -1,8 +1,16 @@
-def str = 'abstract\targuments\tboolean\tbreak\tbyte\tcase\tcatch\tchar\tclass*\tconst\tcontinue\tdebugger\tdefault\tdelete\tdo\tdouble\telse\tenum*\teval\texport*\textends*\tfalse\tfinal\tfinally\tfloat\tfor\tfunction\tgoto\tif\timplements\timport*\tin\tinstanceof\tint\tinterface\tlet\tlong\tnative\tnew\tnull\tpackage\tprivate\tprotected\tpublic\treturn\tshort\tstatic\tsuper*\tswitch\tsynchronized\tthis\tthrow\tthrows\ttransient\ttrue\ttry\ttypeof\tvar\tvoid\tvolatile\twhile\twith\tyield'
+package com.th.js
+//def str = 'abstract\targuments\tboolean\tbreak\tbyte\tcase\tcatch\tchar\tclass*\tconst\tcontinue\tdebugger\tdefault\tdelete\tdo\tdouble\telse\tenum*\teval\texport*\textends*\tfalse\tfinal\tfinally\tfloat\tfor\tfunction\tgoto\tif\timplements\timport*\tin\tinstanceof\tint\tinterface\tlet\tlong\tnative\tnew\tnull\tpackage\tprivate\tprotected\tpublic\treturn\tshort\tstatic\tsuper*\tswitch\tsynchronized\tthis\tthrow\tthrows\ttransient\ttrue\ttry\ttypeof\tvar\tvoid\tvolatile\twhile\twith\tyield'
+//
+//str.split("\t").each {
+//    if (it.endsWith("*"))
+//        it = it.substring(0, it.length() - 1)
+//    print "\"${it}\","
+//}
+//
 
-str.split("\t").each {
-    if (it.endsWith("*"))
-        it = it.substring(0, it.length() - 1)
-    print "\"${it}\","
-}
 
+
+println('b:'+((int)'k'))
+println('k:'+((int)'b'))
+println('m:'+((int)'m'))
+println('g:'+((int)'g'))
