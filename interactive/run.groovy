ed0595b991ddd9fee2cfe6630849bad0c17a5c0e
lyrieek <tianhao7@foxmail.com>
2018-03-30 09:52:32 +0800

    change directory structure

--- interactive/run.groovy
+++ interactive/run.groovy
@@ -1,33 +1,28 @@
-package com.th.js
-
 import com.th.js.container.ContextBlack
 import com.th.js.core.JSFactory
-import com.th.js.core.Status
 import pers.th.util.io.IOUtils
 
 import java.nio.charset.Charset
 
 final String charset = "UTF-8"
 
-static wrap(String item, String style) {
-    return ("<label style='${style}'>${item}</label>")
-}
+def input_path = "C:\\Users\\user\\Desktop\\jquery.js"
+def output_path = "G:\\th\\js.html"
+
+def template_path = "G:\\idea\\JSReader\\html.template"
 
 JSFactory js = new JSFactory()
-js.load(IOUtils.reader("C:\\Users\\user\\Desktop\\jquery.js",
-        Charset.forName(charset)))
+js.load(IOUtils.reader(input_path, Charset.forName(charset)))
 
 StringBuilder buffer = new StringBuilder()
 for (ContextBlack black : js.scanner().codes()) {
-    String item = black.item()
-    item = item.replace("<", "&lt;")
+    String item = black.item().replace("<", "&lt;")
             .replace(">", "&gt;")
             .replace("&", "&amp;")
             .replace(" ", "&nbsp;")
-//			item = StringEscapeUtils.escapeHtml4(item);
     buffer.append(black.status().html(item))
 }
 def content = buffer.toString().replaceAll("\n", "</li>\n<li style='white-space: pre;'>")
-String template = IOUtils.reader("G:\\idea\\JSReader\\html.template", Charset.forName(charset))
+String template = IOUtils.reader(template_path, Charset.forName(charset))
         .replace("\${content}", content)
-IOUtils.write("G:\\th\\js.html", template, Charset.forName(charset))
+IOUtils.write(output_path, template, Charset.forName(charset))
---
+++ test/com/th/js/JSReaderTest.groovy
@@ -0,0 +1,30 @@
+package com.th.js
+
+import com.th.js.container.ContextBlack
+import com.th.js.core.JSFactory
+import pers.th.util.io.IOUtils
+
+import java.nio.charset.Charset
+
+final String charset = "UTF-8"
+
+def input_path = "C:\\Users\\user\\Desktop\\jquery.js"
+def output_path = "G:\\th\\js.html"
+
+def template_path = "G:\\idea\\JSReader\\html.template"
+
+JSFactory js = new JSFactory()
+js.load(IOUtils.reader(input_path, Charset.forName(charset)))
+
+StringBuilder buffer = new StringBuilder()
+for (ContextBlack black : js.scanner().codes()) {
+    String item = black.item().replace("<", "&lt;")
+            .replace(">", "&gt;")
+            .replace("&", "&amp;")
+            .replace(" ", "&nbsp;")
+    buffer.append(black.status().html(item))
+}
+def content = buffer.toString().replaceAll("\n", "</li>\n<li style='white-space: pre;'>")
+String template = IOUtils.reader(template_path, Charset.forName(charset))
+        .replace("\${content}", content)
+IOUtils.write(output_path, template, Charset.forName(charset))
